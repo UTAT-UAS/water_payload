daf4eb7240b75d9a902e74353968c283f12f73e2
Larkteryny <103611388+Larkteryny@users.noreply.github.com>
2025-10-26 15:36:11 -0400

    shouldnt have carried the negative

--- src/main.hpp
+++ src/main.hpp
@@ -6,7 +6,7 @@ int center_us = 1465;
 int increment = 50; // us per step
 int period = 20; // ms per step
 int servo_angle_param = 255; // whatever the parameter is set to in the USB link
-int lbound = -546;
+int lbound = 546;
 int ubound = 2383;
 
 #endif // MAIN_HPP
